99e72f9e550fcb3235f9c15af378ad7f9684a460
o-ray-o <rayfong_goh@mymail.sutd.edu.sg>
2023-12-24 10:22:30 +0800

    first commit

---
+++ exercise/script.js
@@ -0,0 +1,22 @@
+//scene
+const scene = new THREE.Scene();
+
+//Red Cube
+const geometry = new THREE.BoxGeometry(1, 1, 1);
+const material = new THREE.MeshBasicMaterial({ color: "blue" });
+const mesh = new THREE.Mesh(geometry, material);
+scene.add(mesh);
+//sizes
+const sizes = { width: 800, height: 600 };
+
+// Camera
+const camera = new THREE.PerspectiveCamera(75, sizes.width / sizes.height);
+scene.add(camera);
+camera.position.z = 3;
+
+//Renderer
+const canvas = document.querySelector(".webgl");
+const renderer = new THREE.WebGLRenderer({ canvas: canvas });
+renderer.setSize(sizes.width, sizes.height);
+
+renderer.render(scene, camera);
